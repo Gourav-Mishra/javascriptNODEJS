9e563d96e61a4c09a27f76bd4cdc8e318e328070
gourav1995 <gourav.mish1995@gmail.com>
2018-08-29 19:29:47 +0530

    29/8/18

--- utility/utility.js
+++ utility/utility.js
@@ -1,55 +1,68 @@
+/******************************************************************************
+ *  Execution       :   1. default node         
+ *                      
+ *  Purpose         :  LEARNING Functional programs
+ * 
+ *  @description    
+ * 
+ *  @file           : utility.js
+ *  @overview       : hello.js , coin.js , leapcheck.js , powerof2.js , harmonicnumber.js ,primefactor.js ,gambler.js ,coupon.js , 2Darray.js , triplet.js , distance.js , permutation.js , stopwatch.js ,quadratic.js , windchill.js 
+ *  @module         : utility
+ *  @author         : BridgeLabz <GOURAV MISHRA>
+ *  @version        : 1.0
+ *  @since          : 20-08-2018
+ *
+ ******************************************************************************/
+
 const _ = require("lodash")
 module.exports={
-    /*
-     *
-     *  simple hello world program
-     * From user name is taken as inp
-     * 
-     */
-    
-    hello : function(userinput)
+   /**
+    *   NOTE- simple hello world program
+    *        from user name is taken as input and message is displayed.
+    */
+     hello : function(userinput)
     {
-        
-        console.log("type your user name");
         console.log("Hey "+userinput+ " how are you");
     },
     /**
-     * 
-     * @description A year is a  leap Year if it is divisible by 4 and 400 
-     * Every century year is not a leap year
-     * 
-     * 
+     * @param userinput - year is taken as input 
+     * @description     - A year is a  leap Year if it is divisible by 4 and 400 
+     *                    Every century year is not a leap year
      */
 leapcheck: function (userinput)
 {
     
     {
-        if (userinput%400===0 && userinput%4===0)
+        if ((userinput%4===0 && userinput%100!==0 || userinput%400==0) )
+        /** 
+         *  Here  bitwise And operator can also be used.
+         *  (userinput%400===0 & userinput%4===0)
+         */
             {
-                console.log("leap year");
+                console.log("Leap year");
         
             }
         else 
             {
     
-                 console.log("not a leap year");
+                 console.log("Not a Leap year");
             }
     }
     
     
 },
-/*
-* @description 
-* When a coin  is tossed eighter heads or tail comes
-* its non predictable so Random function can be used
-*
-*
-* 
+
+
+/** (COIN)
+* @param userinput - As user input, the number of times we want to flip the coin is taken.                 
+* @description - When a coin  is tossed eighter heads or tail comes
+*              - its non predictable so Random function can be used
+*              - randomFunction returnes value netween 0 and 1
 */
 
 coin: function(userinput)
 {
-    let n=0,x=0,y=0,a,b;
+    let n=0,x=0,y=0;
     
     for(let i=1;i<=userinput;i++)
     {
@@ -65,18 +78,21 @@ coin: function(userinput)
          {
              y++
              console.log("tail");
-         }
+         }// if-else ends
 
-    }
-    var hp=(x/userinput)*100;
+    }// for loop ends
+    var hp=(x/userinput)*100;              // hp is headPercentage
     console.log("the head % is :"+hp+"%");
-    var tp=(y/userinput)*100;
+    var tp=(y/userinput)*100;              //tp is tailPercentage
     console.log("the tail % is :"+tp+"%");
 },
-/*
-* 
-* Using Maths.pow function also the table of power of 2 can be calculated
-* using logic also it can be done.
+
+
+
+/** (POWER OF2)
+* @description Using Maths.pow function also the table of power of 2 can be calculated
+*              using logic also it can be done.
+* @param userinput the number upto which we need the table of 2 is taken as input.
 */
 powerof2:function(userinput)
 {
@@ -88,8 +104,12 @@ powerof2:function(userinput)
         console.log("2 ^ "+i+"="+number);
         count=number;
 
-    }
+    }// for loop ends
 },
+/**
+ * @param userinput the harmonic value N is taken as input
+ * @description its a series addition ,following a simple logic.
+ */
 harmonic: function(userinput)
 {
     var sum=0,i,j,n,c;
@@ -97,13 +117,19 @@ harmonic: function(userinput)
     {
         sum+=1/i;
         
-    }
+    }// for loop ends
     console.log("the harmonic number is "+sum);
 },
-/**
- * user will have a Starting balance using which he will start the game
- * There is a goal set ,which is set ny user, where the game will terminate
- * the game will end if , the user has no money or he has reached goal.
+
+
+
+/**(GAMBLER)
+ * @description  user will have a Starting balance using which he will start the game
+ *               There is a goal set ,which is set by user, where the game will terminate
+ *               the game will end if , the user has no money or he has reached goal.
+ * @param userinput it is the number of times a player want to paly
+ * @param startingbalance its the startingBalance ,the money with the player before every game.
+ * @param goal it is the amount where the player will stop playing 
  * 
  */
 gambler: function(userinput,startingbalance,goal)
@@ -111,11 +137,11 @@ gambler: function(userinput,startingbalance,goal)
     
     var startingbalance,goal,i;
     var win=0,lost=0;
-    if(startingbalance<=goal & startingbalance>=0)
+   if(startingbalance>=0 && startingbalance <=goal)
     {
         for(i=1;i<=userinput;i++)
         {
-            if(startingbalance<goal & startingbalance> 0)
+            if(startingbalance<goal && startingbalance> 0)
             {
                  var n=Math.random();
                  
@@ -128,32 +154,35 @@ gambler: function(userinput,startingbalance,goal)
                 {
                     lost++;
                     console.log("LOST    so current balance= "+(--startingbalance));
-                }
+                }//sub inner if-else ends
             }
             else
             {
                 console.log("sorry <you have have no money> / <you have reached your goal> you cant play")
-            }
+            }// inner if-else ends
 
-        }
+        }// for loop ends
         
 
     } 
-    else
+   else
     {
-        console.log("sorrry you cant play");
-    }
+        console.log("sorrry your startingBalance is less than your goal.");
+    }// outer if-else ends 
     console.log("number of wins:"+win);
     console.log("number of loss:"+lost);
     console.log(" wins % :"+((win/userinput)*100));
     console.log(" loss % :"+((lost/userinput)*100));
-    /*
+    /** 
     * the number of wins and loss are printed
     * similarly the win % and loss % is printed.
     */
     
 },
-/** 
+
+
+
+/** (PRIME FACTOR)
  *  @param number
  * here the input taken is from user
  * here we are using Math.sqrt function
@@ -183,15 +212,14 @@ if(number>2)
 
 }
 console.log(primeFactor);
-
-
-
 },
-/**
- * @param min
- * @param max 
- * NOTE - Here i have used lodash to generate ramdom coupon numbers
- *        To use lodash first we have to install lodash ie: npm i lodash
+
+
+/**(COUPON)
+ * @param min minimun number from which coupon number will start generating
+ * @param max maximum number upto which coupon will be generated
+ * NOTE -     Here i have used lodash to generate ramdom coupon numbers
+ *            To use lodash first we have to install lodash ie: npm i lodash
  *          
  */
 coupon :function(min,max)
@@ -205,18 +233,16 @@ coupon :function(min,max)
         n= _.random(min, max) 
        
         random.push(n);   
-    }
+    }// for loop ends
     console.log(random);
-    console.log("no. of coupon generated= "+random.length);
+    console.log("Number of coupon generated= "+random.length);
     var distinct=new Set(random);
     console.log( distinct);
-    console.log("number of distinct coupon "+[...distinct].length);
-    
-   
- 
-   
+    console.log("Number of distinct coupon "+[...distinct].length);  
 },
-/**
+
+
+/**(DISTANCE)
  * distance form origin(0,0) to the (x,y) coordinate is calculated
  * from user  the (x,y) co-ordinate input is taken.
  * 
@@ -235,14 +261,18 @@ distance: function(x,y)
              var distance=Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
              console.log("The distance from:( "+x +","+ y+") to ( 0,0 ) is: "+distance)
 
-        }
-
+        }// if-else ends
 },
-/**
-*  a,b,c Are the 3 coofficients of the quadrdatfic equation.
-* discriminant is taken as d .
-*
-*/
+
+
+
+/** (QUADRATIC)
+ * @param a coefficient of x^2
+ * @param b coefficient of x
+ * @param c its a constant of the equation 
+ * @param d it is the discriminant
+ * @description - a,b,c Are the 3 coofficients of the quadrdatfic equation.             
+ */
 quadratic: function(a,b,c)
 {
     var a,b,c,d;
@@ -253,7 +283,10 @@ quadratic: function(a,b,c)
     console.log("the 1 st root is :"+x);
     console.log("the 2 nd root is :"+y);
 
-},/**
+},
+
+
+/**(WINDCHILL)
   *
   * @description In this temperature and velocity input is taken.
   * Temperature is in fahrenhite
@@ -277,9 +310,13 @@ windchill: function(temperature,velocity)
     }
 },
 
+
+
 /**(STOPWATCH) :
- * Two function are defined getCurrentTime and getElapsedTime.
- * From new Date() only the time is extracted using getTime().
+ *  @description  Two function are defined getCurrentTime and getElapsedTime.
+ *                From new Date() only the time is extracted using getTime().
+ * @param startTime startingTime of the stopwatch
+ * @param stopTime endingTime of the stopwatch
  * 
  * 
 */
@@ -293,8 +330,8 @@ windchill: function(temperature,velocity)
 
     },
     /** 
-    *NOTES- here the extracted Time is in milliseconds.
-    *      so to convert the required time to seconds it is devided by 1000.
+    * @description here the extracted Time is in milliseconds.
+    *              so to convert the required time to seconds it is devided by 1000.
     * @param startTime
     * @param stopTime
     * 
@@ -306,11 +343,15 @@ windchill: function(temperature,velocity)
 
         return e;
     },
+
+
+
     /**(TRIPLET):
-    * NOTE- Input is taken in array
-    *       random inputs are taken,  only the group whose sum is 3 is returned.
-    *  @param  read
-    *  @param  val
+    * @description Input is taken in array
+    *              random inputs are taken,  only the group whose sum is 3 is returned
+    *              distinct triplets are also extracted using set concept
+    *  @param  read reads the readline statement in utility
+    *  @param  val stores the size of the array input
     * 
     */
 
@@ -344,13 +385,13 @@ windchill: function(temperature,velocity)
                 console.log(" The number of triplets = "+count);
                 if([...distinct].length===0)
                 {
-                    console.log("no distinct triplet found!!");
+                    console.log("NO distinct triplet found!!");
 
                 }
                 else
                 {
-                            console.log("no of distinct triplet found= "+[...distinct].length);
-                            console.log("distinct triplets are: "+[...distinct])
+                            console.log("Number of distinct triplet found= "+[...distinct].length);
+                            console.log("Distinct triplets are: "+[...distinct])
                 }
             }else{
                 read.question(`Enter the ${(arr.length+1)}th value : `,function(ele){
@@ -362,12 +403,11 @@ windchill: function(temperature,velocity)
         }
         findTriplet(val);
     },
-    /*(two-D array)
-     *
-     *  
-     * 
-     * 
-     */
+    /**(TWO D ARRAY) 
+     * @param read calling the read function from twoDarray
+     * @param row enter the number of rows you want
+     * @param col enter the no od cols you want
+    */  
    twoDarray: function(read,row,col)
    {
         var arr=[];
@@ -377,7 +417,7 @@ windchill: function(temperature,velocity)
 
         if(arr.length>=size)
         {
-        {
+        
             var count=0;
             var arr1=[];
             var mat='';
@@ -387,32 +427,30 @@ windchill: function(temperature,velocity)
                 {
                     mat=mat+arr[count]+'';
                     count++;
-                }
+                }//inner for ends
                 arr1.push(mat);
                 mat=''
-             }
+             }// outer for loop ends
              for(var index=0;index<arr1.length;index++)
              {
                  console.log(arr1[index])
-             }
+             }// for loop ends
 
 
-        }
-        
-   }
-   else{
+         }else{
        read.question(`enter teh ${(arr.length)} th value:`,function(result){
-           arr.push(result);
-           createArray(size);
-       });
-   }
+                        arr.push(result);
+                        createArray(size);
+                });
+            }
 }
 createArray(size);
    
 
 },
-/* 2-D Array  using prompt
-* 
+/** (2-D Array  using prompt)
+* @param row the number of rows we want as input 
+* @param promp input method
 *
 */
 array: function(prompt,row)
@@ -428,8 +466,10 @@ array: function(prompt,row)
         }
       }
       console.log(arr);
-},/**
- * @
+},
+/** (PERMUTATION)
+ * @description permutation of string, returns all possible unique set of words which can be made from the given word
+ * @param str   here a string is taken as input.
  */
 permutation: function(str)
 {
@@ -443,11 +483,11 @@ permutation: function(str)
         {
             
             str=str.split('');
-        }
+        }// if ends
         if(str.length==0)
-        {
-            permutation.push(arr.join(''));
-        }
+            {
+                 permutation.push(arr.join(''));
+            }
             for(var i=0;i<str.length;i++)
             {
                 var a= str.splice(i,1);
@@ -455,7 +495,7 @@ permutation: function(str)
                 getPermutation(str,arr);
                 arr.pop();
                 str.splice(i,0,a);
-            }
+            }// inner for loop ends
         }
     }
 }
